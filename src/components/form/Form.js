dd2f4a4fa1abc6da3de982ca4443fc359d86678d
Tyler Corbett <thetylercorbett@gmail.com>
2019-04-13 11:14:43 -0700

    titled slider bars

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -5,10 +5,22 @@ export default function Form() {
     return(
        <div className={styles.all}>
         <h2 className={styles.title}>Form</h2>
-        <input type='range' className={styles.input} min='1' max='100'/>
-        <input type='range' className={styles.input} min='1' max='100'/>
-        <input type='range' className={styles.input} min='1' max='100'/>
-        <input type='range' className={styles.input} min='1' max='100'/>
+        <div>
+            <h3>Garbage</h3>
+            <input type='range' className={styles.input} min='1' max='100'/>
+        </div>
+        <div>
+            <h3>Recycling</h3>
+            <input type='range' className={styles.input} min='1' max='100'/>
+        </div>
+        <div>
+            <h3>Glass Recycling</h3>
+            <input type='range' className={styles.input} min='1' max='100'/>
+        </div>
+        <div>
+            <h3>Yard Waste/Compost</h3>
+            <input type='range' className={styles.input} min='1' max='100'/>
+        </div>
        </div>
     )
 }
