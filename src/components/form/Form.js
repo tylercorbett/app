bca79edaf545649bf3ad1d273779052b20ae77f0
Tyler Corbett <thetylercorbett@gmail.com>
2019-04-13 10:50:01 -0700

    four sliders

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -4,9 +4,11 @@ import styles from './Form.module.css';
 export default function Form() {
     return(
        <div className={styles.all}>
-       <h2 className={styles.title}>Form</h2>
-       <input type='range'>
-       </input>
+        <h2 className={styles.title}>Form</h2>
+        <input type='range' className={styles.input} min='1' max='100'/>
+        <input type='range' className={styles.input} min='1' max='100'/>
+        <input type='range' className={styles.input} min='1' max='100'/>
+        <input type='range' className={styles.input} min='1' max='100'/>
        </div>
     )
 }
