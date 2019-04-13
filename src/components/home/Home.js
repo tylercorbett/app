0b7d671dbd92d6354e7d99c87be72967bc929c38
Tyler Corbett <thetylercorbett@gmail.com>
2019-04-13 10:05:05 -0700

    home done

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,6 +3,16 @@ import styles from './Home.module.css';
 
 export default function Home() {
     return(
-        <h1 className={styles.h1}>Home</h1>
+        <div className={styles.all}>
+            <div className={styles.compost}>
+                <span class='fas fa-apple-alt'/>
+            </div>
+            <div className={styles.recycle}>
+                <span class='fas fa-recycle'/>
+            </div>
+            <div className={styles.trash}>
+                <span class='fas fa-trash'/>
+            </div>
+        </div>
     )
 }
