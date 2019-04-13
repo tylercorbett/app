94355bfb0846d6d604ab1591273293fce93b9f00
Tyler Corbett <thetylercorbett@gmail.com>
2019-04-13 10:41:17 -0700

    background color fullscreen

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -3,8 +3,10 @@ import styles from './Form.module.css';
 
 export default function Form() {
     return(
-       <>
+       <div className={styles.all}>
        <h2 className={styles.title}>Form</h2>
-       </>
+       <input type='range'>
+       </input>
+       </div>
     )
 }
