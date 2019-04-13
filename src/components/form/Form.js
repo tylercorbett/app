1f6bc381d32b2a81452b71c60f5e6c18a1f03121
Tyler Corbett <thetylercorbett@gmail.com>
2019-04-13 10:33:41 -0700

    helloworld state done

---
+++ src/components/form/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Form.module.css';
+
+export default function Form() {
+    return(
+       <>
+       <h2 className={styles.title}>Form</h2>
+       </>
+    )
+}
